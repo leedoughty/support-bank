a6b2b12683e6cbb83511a3147cd1cf5df689ba1a
Lee Doughty <leedoughtydesign@gmail.com>
2020-07-02 11:23:15 +0100

    Configure logger

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const fs = require("fs");
 const parse = require("csv-parse/lib/es5");
 const readlineSync = require("readline-sync");
+const log4js = require("log4js");
+
+log4js.configure({
+  appenders: {
+    file: { type: "fileSync", filename: "logs/debug.log" },
+  },
+  categories: {
+    default: { appenders: ["file"], level: "debug" },
+  },
+});
+
+const logger = log4js.getLogger("debug.log");
 
 function output(err, output) {
   if (err) throw err;
@@ -28,6 +40,7 @@ function output(err, output) {
     accountMap[transaction.to].balance += parseFloat(transaction.amount);
   });
   const userInput = readlineSync.prompt();
+  logger.info(userInput);
 
   if (userInput === "List All") {
     console.log(accountMap);
